a1ddf1a67fa831162ba6f2dc75b8d8831ec76bbc
Melissa Wagasky <melissawagasky@gmail.com>
2018-04-02 13:15:57 -0600

    Import tools for testing

---
+++ test/routes.spec.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const db = require('knex')(configuration);
+
+chai.use(chaiHttp);
