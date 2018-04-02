97938966e8bac92eb2655befe7f84abf4acfc067
Melissa Wagasky <melissawagasky@gmail.com>
2018-04-02 10:05:44 -0600

    Call a function titled postData when a new item is generated

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -12,9 +12,14 @@ const addItem = (event) => {
 
   const name = $('.saveItemForm .nameInput').val();
 
+  postData()
   console.log(name)
 }
 // return the items in the post response for rendering
+
+const postData = () => {
+  console.log('postData was called')
+}
 
 // on selection of a item delete, delete item from database
 // on selection of an item as packed, update database
