524a3a3fd519d980af1d565d756e40669a030605
Melissa Wagasky <melissawagasky@gmail.com>
2018-04-02 13:27:10 -0600

    Add tests for the get endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -32,3 +32,46 @@ describe('Client Routes', () => {
       });
   });
 });
+
+
+describe('API ROUTES', () => {
+  
+  beforeEach(done => {
+    database.migrate.rollback().then(() => {
+      database.migrate.latest().then(() => {
+        return database.seed.run().then(() => {
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('GET /api/v1/items', () => {
+    it('should return all items', () => {
+      return chai
+        .request(server)
+        .get('/api/v1/items/')
+        .then(response => {
+          expect(response).to.have.status(200);
+          expect(response.body).to.be.a('array');
+          expect(response.body[0]).to.have.property('id');
+          expect(response.body[0]).to.have.property('name');
+          expect(response.body[0]).to.have.property('packed');
+        })
+        .catch(error => {
+          throw error;
+        });
+    });
+
+
+    it('should not return items if called with the wrong params', () => {
+      return chai
+        .request(server)
+        .get('/api/v1/items$%##*&$^#')
+        .then(response => {
+          expect(response).to.have.status(404);
+        });
+    });
+  });
+});
