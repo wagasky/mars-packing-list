6ba8c23db3ef42a86cecb4984a04095e48520f95
Melissa Wagasky <melissawagasky@gmail.com>
2018-04-02 13:22:38 -0600

    Add seed files for testing and initial tests for the client route

---
+++ db/seeds/test/initial.js
@@ -0,0 +1,14 @@
+exports.seed = function(knex, Promise) {
+ // Deletes ALL existing entries
+ return knex('items').del()
+     .then(function () {
+       return Promise.all([
+         knex('items').insert({
+           name: 'Test Packing Item Name',
+           packed: false
+         }, 'id')
+         .then(() => console.log('Seeding complete!'))
+         .catch( error => console.log(`Error seeding data: ${error}`))
+       ])
+   })
+};
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -5,6 +5,30 @@ const chaiHttp = require('chai-http');
 const server = require('../server');
 const environment = process.env.NODE_ENV || 'test';
 const configuration = require('../knexfile')[environment];
-const db = require('knex')(configuration);
+const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  beforeEach(done => {
+    database.migrate.rollback().then(() => {
+      database.migrate.latest().then(() => {
+        return database.seed.run().then(() => {
+          done();
+        });
+      });
+    });
+  });
+
+  it('should return 404', () => {
+    return chai
+      .request(server)
+      .get('/sad')
+      .then(response => {
+        response.should.have.status(404);
+      })
+      .catch(error => {
+        throw error;
+      });
+  });
+});
