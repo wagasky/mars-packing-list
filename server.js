b4443280d66b09632b16b94abdf74fe93d63cfab
Melissa Wagasky <melissawagasky@gmail.com>
2018-04-02 09:46:08 -0600

    Setup server file structure and knexfile structure

---
+++ server.js
@@ -0,0 +1,22 @@
+const $ = require("jquery");
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 3000);
+app.locals.title = "Mars Packing List";
+
+app.get('/api/v1/projects', (request, response) => {
+  
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+});
+
+module.exports = app;
